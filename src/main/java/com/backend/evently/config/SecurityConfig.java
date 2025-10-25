01f7fbdf8330220845a508857bda75ae828556cf
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2025-10-25 18:12:59 -0300

    Criando rota para teste de saúde da API e liberando sem autenticação

---
+++ src/main/java/com/backend/evently/config/SecurityConfig.java
@@ -0,0 +1,26 @@
+package com.backend.evently.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+
+                        .requestMatchers(HttpMethod.GET, "/api/health-check").permitAll()
+
+                        .anyRequest().authenticated()
+                );
+
+        return http.build();
+    }
+}
