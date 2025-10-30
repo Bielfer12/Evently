ba91629947632d31ed1d8956e756a2f9f63f94e1
Guilherme <127777746+GuilhermeCarrer@users.noreply.github.com>
2025-10-30 15:33:29 -0300

    Add files via upload

---
+++ src/main/java/com/backend/evently/config/AuditConfig.java
@@ -0,0 +1,18 @@
+package com.backend.evently.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.AuditorAware;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+import java.util.UUID;
+
+@Configuration
+@EnableJpaAuditing(auditorAwareRef = "auditorAwareImpl")
+public class AuditConfig {
+
+    @Bean
+    public AuditorAware<UUID> auditorAwareImpl() {
+        return new AuditorAwareImpl();
+    }
+}
