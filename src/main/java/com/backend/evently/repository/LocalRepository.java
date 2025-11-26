4f9ad2ec12382f69173cdba58dcdba7ee49a13a1
Gabriel Casagrande <126476989+Bielfer12@users.noreply.github.com>
2025-11-26 18:21:28 -0300

    repository local

--- src/main/java/com/backend/evently/repository/LocalRepository.java
+++ src/main/java/com/backend/evently/repository/LocalRepository.java
@@ -1,9 +1,11 @@
 package com.backend.evently.repository;
 
 import com.backend.evently.model.Local;
+import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.UUID;
 
 public interface LocalRepository extends JpaRepository<Local, UUID> {
+    boolean existsBySlug(@NotBlank(message = "O slug é obrigatório") String slug);
 }
