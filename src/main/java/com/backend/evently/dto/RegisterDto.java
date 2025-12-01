5506d8c1c1d7b73b323e6def64336dd74d7b1c1d
Jean Vitor Vieira <jeanvitorv0@unesc.net>
2025-12-01 20:14:03 -0300

    Ajustando validação de senha

--- src/main/java/com/backend/evently/dto/RegisterDto.java
+++ src/main/java/com/backend/evently/dto/RegisterDto.java
@@ -1,13 +1,18 @@
 package com.backend.evently.dto;
 
 import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
 
 public record RegisterDto(
         @Email
         String email,
-        @Min(value = 8, message = "senha deve possuir no mínimo 8 caracteres")
+        @Size(min = 8, message = "senha deve possuir no mínimo 8 caracteres")
+        @Pattern(
+                regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]+$",
+                message = "senha deve conter pelo menos 1 letra, 1 número e 1 caractere especial"
+        )
         String password,
         @NotBlank(message = "nome é obrigatório") String nome
 ) {}
